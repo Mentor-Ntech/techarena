9cbe2fd629c7daf086bc525575fee2b12b9d09bf
Mentor-Ntech <oloyedeaderayo11@gmail.com>
2022-09-15 20:08:16 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    
+
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+    
+  });
+});
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: true,
+  grabCursor: true,
+  fade: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+    0: {
+      slidesPerView: 1,
+    },
+    520:{
+      slidesPerView: 2, 
+    },
+    950:{
+      slidesPerView: 3,
+    }
+  }
+});
+
